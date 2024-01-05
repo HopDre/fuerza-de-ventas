0779909bf9e882c1c6889516f260fdba5446c9b4
Felipe <felipesanchezp24@gmail.com>
2024-01-05 17:43:47 -0300

    05-01-2024

---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+let currentSlide = 0;
+  let intervalId;
+
+  function showSlide(index) {
+    const slider = document.getElementById('slider');
+    const paginationDots = document.querySelectorAll('.pagination-dot');
+    
+    currentSlide = index;
+    slider.style.transform = `translateX(${-index * 100}%)`;
+
+    paginationDots.forEach((dot, i) => {
+      dot.classList.toggle('active', i === index);
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % 5;
+    showSlide(currentSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + 5) % 5;
+    showSlide(currentSlide);
+  }
+
+  function startAutoSlide() {
+    intervalId = setInterval(() => {
+      nextSlide();
+    }, 5000); // Cambia de slide cada 5 segundos (puedes ajustar el intervalo según tus necesidades)
+  }
+
+  function stopAutoSlide() {
+    clearInterval(intervalId);
+  }
+
+  // Crear los puntos de paginación
+  const paginationContainer = document.getElementById('slider-pagination');
+  for (let i = 0; i < 5; i++) {
+    const dot = document.createElement('div');
+    dot.classList.add('pagination-dot');
+    dot.onclick = () => {
+      stopAutoSlide();
+      showSlide(i);
+    };
+    paginationContainer.appendChild(dot);
+  }
+  startAutoSlide(); // Iniciar el movimiento automático
+
+
+   // Inicializar ScrollReveal
+   ScrollReveal().reveal('.scroll-reveal-section', {
+    delay: 200,
+    distance: '50px',
+    easing: 'ease-in-out',
+    origin: 'bottom',
+    reset: true
+});
